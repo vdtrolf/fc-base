596fbe6dd437a7be24c7be1f55cfef4c2e1364bb
Rolf van der Tang <96309486+vdtrolf@users.noreply.github.com>
2024-02-16 03:34:33 +0100

    clean js

--- dist/data/repository/createName.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createName = void 0;
-const createName = (dbHelper, name) => {
-    return dbHelper.putItem("person_names", name, name.id);
-};
-exports.createName = createName;
-//# sourceMappingURL=createName.js.map
